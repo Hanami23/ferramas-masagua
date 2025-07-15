5c4bfd1ac8bd40c7d20f1475ef333ea0fcf35983
Hanami23 <165859450+Hanami23@users.noreply.github.com>
2025-07-15 00:31:20 -0400

    Delete MasAgua directory

--- MasAgua/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import TipoAgua, Producto, Boleta, DetalleBoleta
-
-# Register your models here.
-admin.site.register(TipoAgua)
-admin.site.register(Producto)
-admin.site.register(Boleta)
-admin.site.register(DetalleBoleta)
